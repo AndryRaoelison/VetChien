bf410240152f64e549430d05909a5164f05e9e77
AndryRaoelison <andrysololalao17@gmail.com>
2024-11-02 17:39:09 +0300

    mergin from main and added animation header

--- main.js
+++ main.js
@@ -13,3 +13,39 @@ navLinks.addEventListener("click", () => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+//animation apparation effect
+function revealAuto(content, delay) {
+  const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  ScrollReveal().reveal(content, {
+    ...scrollRevealOption,
+    delay: delay,
+  });
+}
+const scrollRevealLists = [
+  ".header__content h4 ",
+  ".header__content h1",
+  ".header__content h2",
+  ".header__btn",
+  ".header__content p",
+];
+let delay = 500;
+scrollRevealLists.forEach((revealList) => {
+  revealAuto(revealList, delay);
+  delay += 250;
+});
+
+//factorisation up there ;)
+// const scrollRevealOption = {
+//   distance: "50px",
+//   origin: "bottom",
+//   duration: 1000,
+// };
+
+// ScrollReveal().reveal(".header__content h4 ", {
+//   ...scrollRevealOption,
+// });
